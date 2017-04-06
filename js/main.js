9af548835c5194c4cd3cb89bcbfbd19940a8ec4f
Lisa Long <garrisonridge@gmail.com>
2017-04-05 23:54:52 -0400

    Add hamburger css + js code to construct menu icon

--- js/main.js
+++ js/main.js
@@ -17,6 +17,62 @@ $(document).ready(function () {
         // configuration
         offCanvas: {
             pageSelector: '#wrapper'
+        },
+        classNames: {
+            fixedElements: {
+                fixed: 'fixed',
+                ElemInsertMethod: 'prependTo',
+                elemInsertSelector: 'header'
+            }
+        }
+    });
+    var menuButton = $('#menu-button');
+    var mainMenu = $('#menu').data('mmenu');
+
+    $('#menu-button').click(function () {
+        mainMenu.open();
+    });
+
+    menuButton.on('click', function () {
+        mainMenu.open();
+    });
+
+    mainMenu.bind('opened', function () {
+        setTimeout(function () {
+            menuButton.addClass('is-active');
+        }, 100);
+    });
+
+    mainMenu.bind('closed', function () {
+        setTimeout(function () {
+            menuButton.removeClass('is-active');
+        }, 100);
+    });
+
+
+});
+
+
+/*
+$(document).ready(function () {
+    //mmenu - plugin for hamburger menu
+    $('#menu').mmenu({
+        // options
+        // preventDefault is needed because href points to # on same page
+        // if menu linked to other pages, would not need to set
+        // preventDefault to false
+        //close: true needed because when did above, only, menu stayed open
+        onClick: {
+            close: true,
+            preventDefault: null,
+        },
+        navbar: {
+            title: "Gusteau's"
+        }
+    }, {
+        // configuration
+        offCanvas: {
+            pageSelector: '#wrapper'
         }
     });
 
@@ -28,3 +84,4 @@ $(document).ready(function () {
 
 
 });
+*/
