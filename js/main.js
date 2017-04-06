aba5a0a51b0b39dec69d2fef3567c5c7c39a8be4
Lisa Long <garrisonridge@gmail.com>
2017-04-05 21:49:41 -0400

    mmenu js options + configuration

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 $(document).ready(function () {
-    $('#menu').mmenu();
-
-    // configuration
-    offCanvas: {
-        pageSelector: '#wrapper'
+    //mmenu - plugin for hamburger menu
+    $('#menu').mmenu({
+        // options
+        // preventDefault is needed because href points to # on same page
+        // if menu linked to other pages, would not need to set
+        // preventDefault to false
+        //close: true needed because when did above, only, menu stayed open
+        onClick: {
+            close: true,
+            preventDefault: null,
+        },
+        navbar: {
+            title: "Gusteau's"
         }
+    }, {
+        // configuration
+        offCanvas: {
+            pageSelector: '#wrapper'
+        }
+    });
+
+    var mainMenu = $('#menu').data('mmenu');
+
+    $('#menu-button').click(function () {
+        mainMenu.open();
+    });
+
 
 });
