aa57d2be5c9198b0be8341100bd3fc9a50173df5
Lisa Long <garrisonridge@gmail.com>
2017-04-11 22:50:06 -0400

    Add js for monthly ad + style

--- js/main.js
+++ js/main.js
@@ -64,36 +64,14 @@ $(document).ready(function () {
 
 });
 
+//javascript custom code
+window.onload = runAd;
 
-/*
-$(document).ready(function () {
-    //mmenu - plugin for hamburger menu
-    $('#menu').mmenu({
-        // options
-        // preventDefault is needed because href points to # on same page
-        // if menu linked to other pages, would not need to set
-        // preventDefault to false
-        //close: true needed because when did above, only, menu stayed open
-        onClick: {
-            close: true,
-            preventDefault: null,
-        },
-        navbar: {
-            title: "Gusteau's"
-        }
-    }, {
-        // configuration
-        offCanvas: {
-            pageSelector: '#wrapper'
-        }
-    });
+function runAd() {
+    var adBox = 'monthlyAd';
 
-    var mainMenu = $('#menu').data('mmenu');
-
-    $('#menu-button').click(function () {
-        mainMenu.open();
-    });
-
-
-});
-*/
+    document.getElementById(adBox).style.display = 'block';
+    document.getElementById('closeBox').onclick = function () {
+        document.getElementById(adBox).style.display = 'none';
+    };
+}
